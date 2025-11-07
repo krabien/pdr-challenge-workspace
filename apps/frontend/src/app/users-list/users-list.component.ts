352df27832b0d9333b08507cbd569852869b72e2
Kai Rabien <krabien@gmail.com>
2025-11-07 17:24:41 +0100

    PCC-14 - Sorting and Pagination

--- apps/frontend/src/app/users-list/users-list.component.ts
+++ apps/frontend/src/app/users-list/users-list.component.ts
@@ -1,9 +1,11 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { AsyncPipe, DatePipe } from '@angular/common';
-import { MatTableModule } from '@angular/material/table';
+import { ChangeDetectionStrategy, Component, OnDestroy, ViewChild } from '@angular/core';
+import { AsyncPipe } from '@angular/common';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
 import { UsersStoreService } from '../users-storage.service';
 import { User } from '@pdr-challenge-workspace/shared';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-users-list',
@@ -11,16 +13,16 @@ import { User } from '@pdr-challenge-workspace/shared';
   imports: [
     // Angular
     AsyncPipe,
-    DatePipe,
     // Angular Material
     MatTableModule,
     MatProgressSpinnerModule,
+    MatPaginatorModule,
   ],
   templateUrl: './users-list.component.html',
   styleUrl: './users-list.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class UsersListComponent {
+export class UsersListComponent implements OnDestroy {
   // Define the columns to display in the material table
   readonly displayedColumns: (keyof User)[] = [
     'id',
@@ -30,5 +32,32 @@ export class UsersListComponent {
     'role',
   ];
 
-  constructor(public readonly usersService: UsersStoreService) {}
+  readonly dataSource = new MatTableDataSource<User>([]);
+
+  // because the table is inside an @if block, we need to use a view child to get the paginator
+  @ViewChild(MatPaginator)
+  set paginator(p: MatPaginator | undefined) {
+    if (p) {
+      this.dataSource.paginator = p;
+    }
+  }
+
+  private sub = new Subscription();
+
+  constructor(public readonly usersService: UsersStoreService) {
+    // Feed data into the table's data source
+    this.sub.add(
+      this.usersService.users$.subscribe((users) => {
+        this.dataSource.data = users ?? [];
+      })
+    );
+  }
+
+  ngAfterViewInit(): void {
+    this.dataSource.paginator = this.paginator;
+  }
+
+  ngOnDestroy(): void {
+    this.sub.unsubscribe();
+  }
 }
