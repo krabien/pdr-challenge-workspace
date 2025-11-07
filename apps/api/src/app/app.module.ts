c7c471258cf6d17533aed6a58bb012c649b46102
Kai Rabien <krabien@gmail.com>
2025-11-07 10:21:24 +0100

    cleanup

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -1,13 +1,11 @@
-import {Module} from '@nestjs/common';
-import {AppController} from './app.controller';
-import {AppService} from './app.service';
-import {UsersModule} from '../users/users.module';
-import {UsersController} from '../users/users.controller';
-import {UsersService} from '../users/users.service';
+import { Module } from '@nestjs/common';
+import { UsersModule } from '../users/users.module';
+import { UsersController } from '../users/users.controller';
+import { UsersService } from '../users/users.service';
 
 @Module({
   imports: [UsersModule],
-  controllers: [AppController, UsersController],
-  providers: [AppService, UsersService],
+  controllers: [UsersController],
+  providers: [UsersService],
 })
 export class AppModule {}
--- apps/api/src/app/app.service.ts
+++
@@ -1,9 +0,0 @@
-import {Injectable} from '@nestjs/common';
-import {shared} from '@pdr-challenge-workspace/shared';
-
-@Injectable()
-export class AppService {
-  getData(): { message: string } {
-    return { message: `Hello, API! shared is: ${shared()}` };
-  }
-}
