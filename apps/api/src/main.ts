5010909bd891511bd5307c6037bc181c5a654775
Kai Rabien <krabien@gmail.com>
2025-11-07 16:51:03 +0100

    CORS: allow all origins

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -9,6 +9,10 @@ import { AppModule } from './app/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // NOTE: This is not a production server, in our controlled environment, we allow all origins.
+  app.enableCors({ origin: '*'});
+
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
   const port = process.env.PORT || 3000;
